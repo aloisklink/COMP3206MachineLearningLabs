4d4e0762789544f610104db87d4020c943cb33e1
Alois Klink <alois.klink@gmail.com>
2016-12-11 01:31:00 +0000

    Finishes off Linear Regression Chaotic Time Series
    Tidies up code slightly and cleans up the graph

--- Coursework/timeseriesPrediction.m
+++ Coursework/timeseriesPrediction.m
@@ -1,13 +1,17 @@
-%% Time Series Prediction
+%% Chaotic Time Series Prediction
+% Using the Mackey-Glass Time Series
+
+% creates the time series
 [samples, times] = createMackeySamples(2000);
-p = 20;
 
 % converts the time series to input/output for machine learning
 training_range = 1:1500;
 testing_range = 1501:2000;
+p = 20; % number of past data to use as input
 [train_data, test_data] = ...
 	convertTimeSeriesToMLInput(samples, times, p, training_range, testing_range);
 
+%% Linear Regression
 % Least squares regression as pseudo inverse
 leastSquareReg = @(Ytr, Ftr, Yinput) ...
     [Yinput ones(length(Yinput),1)]* ...
@@ -18,6 +22,13 @@ test_data_predicted = leastSquareReg(train_data.in, train_data.out, test_data.in
 
 squareError = @(Factual, Fpredict) ...
 	(Factual - Fpredict).'*(Factual-Fpredict);
-disp(['Square Error is: ' num2str(squareError(test_data.out, test_data_predicted))]);
+disp(['Square Error of Linear Regression Prediction is: ' num2str(squareError(test_data.out, test_data_predicted))]);
 
-plot(test_data.time, test_data.out, test_data.time, test_data_predicted);
+figure(1);
+plot(test_data.time, test_data.out, 'b--', 'DisplayName', 'Expected Data');
+hold on;
+plot(test_data.time, test_data_predicted, 'r-.', 'DisplayName', 'Linear Regression Prediction');
+legend('show');
+title('Mackey-Glass Time Series Prediction');
+xlabel('Time');
+hold off;
