beedb359185b03d40a77fbd50cf677d9c90ec061
Alois Klink <alois.klink@gmail.com>
2016-12-11 01:15:56 +0000

    Moves train/test data creation code into own function
    Moves the code that was used to generate training and testing data
    from the time series into its own function:
    	convertTimeSeriesToMLInput.m

---
+++ Coursework/convertTimeSeriesToMLInput.m
@@ -0,0 +1,53 @@
+function [ trainData, testData ] = convertTimeSeriesToMLInput( data, time, p, train_range, test_range )
+%convertTimeSeriesToMLInput Converts a time series to input/output data
+%   Converts a time series to training input/output data and testing
+%   input/ouptut data. 
+%   For example, converts [1 2 3 4 5 6 7 8 9 10] to (if p = 5):
+%    input:        output:
+%   [ 1 2 3 4 5;   [ 6;
+%     2 3 4 5 6;     7;
+%	  3 4 5 6 7;     8;
+%	  4 5 6 7 8;     9;
+%	  5 6 7 8 9 ]    10 ]
+
+% tr_tst(1) contains training data
+% tr_tst(2) contains testing data
+tr_tst(1).range = train_range;
+tr_tst(2).range = test_range;
+
+% for training or testing
+for tr_or_tst = [1 2]
+	
+	% data_size is the size of the data
+	% used to cut down the size of the array at the end
+	data_size = 0;
+	
+	max_length = length(tr_tst(tr_or_tst).range);
+	tr_tst(tr_or_tst).data.in = zeros(max_length, p);
+	tr_tst(tr_or_tst).data.out = zeros(max_length, 1);
+	tr_tst(tr_or_tst).data.time = zeros(max_length, 1);
+	
+	for i = tr_tst(tr_or_tst).range
+		if(i <= p)
+			% can't do anything since too close to beginning of the array
+		else
+			data_size = data_size + 1;
+			% input p data values
+			tr_tst(tr_or_tst).data.in(data_size,:) = data( (i - p): (i - 1));
+			% output data value
+			tr_tst(tr_or_tst).data.out(data_size) = data(i);
+			% time of the output
+			tr_tst(tr_or_tst).data.time(data_size) = time(i);
+		end
+	end
+	
+	% cuts the unused rows of the matrices out
+	tr_tst(tr_or_tst).data.in   = tr_tst(tr_or_tst).data.in(1:data_size,:);
+	tr_tst(tr_or_tst).data.out  = tr_tst(tr_or_tst).data.out(1:data_size);
+	tr_tst(tr_or_tst).data.time = tr_tst(tr_or_tst).data.time(1:data_size);
+end
+
+trainData = tr_tst(1).data;
+testData  = tr_tst(2).data;
+
+end
--- Coursework/timeseriesPrediction.m
+++ Coursework/timeseriesPrediction.m
@@ -1,37 +1,12 @@
 %% Time Series Prediction
 [samples, times] = createMackeySamples(2000);
 p = 20;
-% create training data
-train_data_max = 1500;
-train_data_min = 1;
 
-train_data_actual_min = train_data_min + p;
-train_data_size = train_data_max-train_data_actual_min;
-% input 
-train_data.in = zeros(train_data_size, p);
-train_data.out = zeros(train_data_size, 1);
-j = 1;
-for i = train_data_actual_min:train_data_max
-	% adds 1 for offset
-	train_data.in(j,:) = samples( (i - p): (i - 1));
-	train_data.out(j) = samples(i);
-	train_data.time(j) = times(i);
-	j = j + 1;
-end
-% create testing data
-test_data_max = 2000;
-test_data_min = train_data_max + 1;
-test_data_size = test_data_max - test_data_min;
-test_data.in = zeros(test_data_size, p);
-test_data.out = zeros(test_data_size, 1);
-j = 1;
-for i = test_data_min:test_data_max
-	% adds 1 for offset
-	test_data.in(j,:) = samples( (i - p): (i - 1));
-	test_data.out(j) = samples(i);
-	test_data.time(j) = times(i);
-	j = j + 1;
-end
+% converts the time series to input/output for machine learning
+training_range = 1:1500;
+testing_range = 1501:2000;
+[train_data, test_data] = ...
+	convertTimeSeriesToMLInput(samples, times, p, training_range, testing_range);
 
 % Least squares regression as pseudo inverse
 leastSquareReg = @(Ytr, Ftr, Yinput) ...
